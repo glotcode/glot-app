cf912a43e274bad0af68ae9ff711ea0242201436
Petter Rasmussen <petter@hask.no>
2022-09-07 12:45:44 +0300

    Replace inline svg with heroicons functions

--- glot_core/src/layout/app_layout.rs
+++ glot_core/src/layout/app_layout.rs
@@ -1,3 +1,4 @@
+use crate::icons::heroicons;
 use maud::html;
 use maud::Markup;
 use polyester::browser::DomId;
@@ -63,9 +64,8 @@ where
                                     span class="sr-only" {
                                         "Close sidebar"
                                     }
-                                    svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" {
-                                        path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" {
-                                        }
+                                    span class="h6 w-6 text-white" {
+                                        (heroicons::x_mark())
                                     }
                                 }
                             }
@@ -75,18 +75,16 @@ where
                                 }
                                 nav class="mt-5 px-2 space-y-1" {
                                     a class="bg-gray-900 text-white group flex items-center px-2 py-2 text-base font-medium rounded-md" href="#" {
-                                        svg class="text-gray-300 mr-4 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" {
-                                            path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" {
-                                            }
+                                        span class="text-gray-300 mr-4 flex-shrink-0 h-6 w-6" {
+                                            (heroicons::home())
                                         }
                                         "Home"
                                     }
                                     a class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-base font-medium rounded-md" href="#" {
-                                        svg class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" {
-                                            path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" {
-                                            }
+                                        span class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6" {
+                                            (heroicons::pencil_square_solid())
                                         }
-                                        "Snippet"
+                                        "New"
                                     }
                                 }
                             }
@@ -105,18 +103,17 @@ where
                         }
                         nav class="mt-5 flex-1 px-2 space-y-1" {
                             a class="text-gray-300 group flex items-center px-2 py-2 text-sm font-medium rounded-md" href="#" {
-                                svg class="text-gray-300 mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" {
-                                    path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" {
-                                    }
+                                span class="text-gray-300 mr-3 flex-shrink-0 h-6 w-6" {
+                                    (heroicons::home())
                                 }
                                 "Home"
                             }
                             a class="bg-gray-900 text-white text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md" href="#" {
-                                svg class="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" {
-                                    path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" {
-                                    }
+                                span class="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6" {
+                                    (heroicons::pencil_square_solid())
                                 }
                                 "New"
+
                             }
                         }
                     }
@@ -128,9 +125,8 @@ where
                         span class="sr-only" {
                             "Open sidebar"
                         }
-                        svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" {
-                            path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" {
-                            }
+                        span class="h-6 w-6" {
+                            (heroicons::bars_3())
                         }
                     }
                 }
