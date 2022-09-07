e0c68b9d7b9e2963a8df1bafd657d6ef18049659
Petter Rasmussen <petter@hask.no>
2022-09-07 15:01:19 +0300

    Make sidebar less wide

--- glot_core/src/layout/app_layout.rs
+++ glot_core/src/layout/app_layout.rs
@@ -94,7 +94,7 @@ where
                 }
             }
 
-            div class="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0" {
+            div class="hidden md:flex md:w-60 md:flex-col md:fixed md:inset-y-0" {
                 div class="flex-1 flex flex-col min-h-0 bg-gray-800" {
                     div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto" {
                         div class="flex items-center flex-shrink-0 px-4" {
