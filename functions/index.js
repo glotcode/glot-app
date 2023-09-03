406b88959df0b0503fbca617bc598aa861efda60
Petter Rasmussen <petter@hask.no>
2023-09-03 19:35:51 +0200

    Test wasm

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,12 @@
-export function onRequest(context) {
-  return new Response("Hello, world!");
+//import wasmModule from "../../pkg/distance.wasm";
+import init, { homePage } from "../dist/wasm/glot";
+
+export async function onRequest({ request }) {
+  await init("../dist/wasm/glot");
+
+  //const moduleInstance = await WebAssembly.instantiate(wasmModule);
+  //const distance = await moduleInstance.exports.distance_between();
+  //console.log(moduleInstance.exports);
+
+  return new Response("Hello, world2!");
 }
