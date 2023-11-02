1785bb878cb3003bfb4bbefd9d31e971e7ac28f7
Petter Rasmussen <petter@hask.no>
2023-11-02 12:51:20 +0100

    Decode top Option<WindowSize>

--- glot_wasm/src/snippet_page.rs
+++ glot_wasm/src/snippet_page.rs
@@ -1,4 +1,5 @@
 use glot_core::page::snippet_page;
+use poly::browser;
 use poly::page::wasm;
 use poly::page::Page;
 use poly_macro::impl_wasm_page;
@@ -9,16 +10,16 @@ pub struct SnippetPage(snippet_page::SnippetPage);
 
 impl_wasm_page!(SnippetPage);
 
-#[wasm_bindgen(js_name = snippetPage)]
+#[wasm_bindgen(js_name = "snippetPage")]
 pub fn new(js_window_size: JsValue, js_current_url: JsValue) -> Result<SnippetPage, JsValue> {
-    let window_size = wasm::decode_js_value(js_window_size)
+    let window_size: Option<browser::WindowSize> = wasm::decode_js_value(js_window_size)
         .map_err(|err| format!("Failed to decode window size: {}", err))?;
 
     let current_url = wasm::decode_js_value(js_current_url)
         .map_err(|err| format!("Failed to decode URL: {}", err))?;
 
     Ok(SnippetPage(snippet_page::SnippetPage {
-        window_size: Some(window_size),
+        window_size,
         current_url,
     }))
 }
