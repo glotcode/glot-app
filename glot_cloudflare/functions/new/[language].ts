ec45cc651c56b48a041b80c15b3074342cde15af
Petter Rasmussen <petter@hask.no>
2023-11-02 12:50:41 +0100

    Use typescript

--- glot_cloudflare/functions/new/[language].ts
+++ glot_cloudflare/functions/new/[language].ts
@@ -1,11 +1,7 @@
 import glot from "../../dist_backend/wasm_backend/glot";
 
 export async function onRequest({ request }) {
-  // TODO: add function? glot.emptyWindowSize() -> None
-  const page = glot.snippetPage(
-    { width: 800, height: 600 },
-    "http://example.com/new/rust"
-  );
+  const page = glot.snippetPage(null, "http://example.com/new/rust");
 
   const { model, effects } = page.init();
   const html = page.view(model);
